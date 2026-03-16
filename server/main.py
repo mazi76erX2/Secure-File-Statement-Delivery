57de2f3ad00cf829588b23c462b0ab837518c98d
Xolani Mazibuko <mazi76erx@gmail.com>
2026-03-16 22:55:54 +0200

    Minor changes for setup

--- server/main.py
+++ server/main.py
@@ -5,6 +5,7 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from api import api_router
+from cache import redis_cache
 from config import configure_logging, settings
 from database import engine
 from debug_toolbar.middleware import DebugToolbarMiddleware
@@ -15,15 +16,16 @@ configure_logging(settings.log_level)
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
-    del app
+async def lifespan(_app: FastAPI) -> AsyncGenerator[None]:
+    await redis_cache.connect()
     yield
+    await redis_cache.close()
     await engine.dispose()
 
 
 app = FastAPI(
     title="Server API",
-    description="An API for FastAPI with PostgreSQL and Redis.",
+    description="An API for Secure File Statement Delivery.",
     version="0.1.0",
     debug=settings.debug,
     lifespan=lifespan,
