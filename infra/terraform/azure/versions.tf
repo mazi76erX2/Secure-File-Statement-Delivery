72e7edf366bf36c1b8bfc49a45abc203eccbb576
Xolani Mazibuko <mazi76erx@gmail.com>
2026-03-21 16:19:19 +0200

    Add Azure remote backend and ARM env wiring

--- infra/terraform/azure/versions.tf
+++ infra/terraform/azure/versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstatemazi"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
