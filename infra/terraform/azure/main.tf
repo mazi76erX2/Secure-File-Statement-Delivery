861bc8babef30f0b703246b2a8ea197eb3b934e1
Xolani Mazibuko <mazi76erx@gmail.com>
2026-03-21 17:14:01 +0200

    Fix Postgres zone lifecycle

--- infra/terraform/azure/main.tf
+++ infra/terraform/azure/main.tf
@@ -90,6 +90,10 @@ resource "azurerm_postgresql_flexible_server" "main" {
     active_directory_auth_enabled = false
     password_auth_enabled         = true
   }
+
+  lifecycle {
+    ignore_changes = [zone]
+  }
 }
 
 resource "azurerm_postgresql_flexible_server_database" "app" {
